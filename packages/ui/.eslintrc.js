3384908a91f8b1641ae5a17338b59def0ccd8810
coobeet <coobeet@gmail.com>
2023-12-16 11:20:32 +0800

    chore: lint files

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,9 +1,11 @@
-/** @type {import("eslint").Linter.Config} */
+const { resolve } = require('node:path');
+
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/react-internal.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.lint.json",
+    project: resolve(__dirname, 'tsconfig.lint.json'),
   },
 };
