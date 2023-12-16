ede216bebe7de92b933965f61a73ab8651d983e0
coobeet <coobeet@gmail.com>
2023-12-16 17:13:43 +0800

    feat: restore docs files

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@repo/ui/button';
-import { Card } from '@repo/ui/card';
-import { Code } from '@repo/ui/code';
-import Image from 'next/image';
-import styles from './page.module.css';
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Code } from "@repo/ui/code";
+import styles from "./page.module.css";
+import { Button } from "@repo/ui/button";
 
 function Gradient({
   conic,
@@ -22,32 +22,32 @@ function Gradient({
         className,
       ]
         .filter(Boolean)
-        .join(' ')}
+        .join(" ")}
     />
   );
 }
 
 const LINKS = [
   {
-    title: 'Docs',
-    href: 'https://turbo.build/repo/docs',
-    description: 'Find in-depth information about Turborepo features and API.',
+    title: "Docs",
+    href: "https://turbo.build/repo/docs",
+    description: "Find in-depth information about Turborepo features and API.",
   },
   {
-    title: 'Learn',
-    href: 'https://turbo.build/repo/docs/handbook',
-    description: 'Learn more about monorepos with our handbook.',
+    title: "Learn",
+    href: "https://turbo.build/repo/docs/handbook",
+    description: "Learn more about monorepos with our handbook.",
   },
   {
-    title: 'Templates',
-    href: 'https://turbo.build/repo/docs/getting-started/from-example',
-    description: 'Choose from over 15 examples and deploy with a single click.',
+    title: "Templates",
+    href: "https://turbo.build/repo/docs/getting-started/from-example",
+    description: "Choose from over 15 examples and deploy with a single click.",
   },
   {
-    title: 'Deploy',
-    href: 'https://vercel.com/new',
+    title: "Deploy",
+    href: "https://vercel.com/new",
     description:
-      ' Instantly deploy your Turborepo to a shareable URL with Vercel.',
+      " Instantly deploy your Turborepo to a shareable URL with Vercel.",
   },
 ];
 
@@ -65,7 +65,7 @@ export default function Page(): JSX.Element {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{' '}
+            By{" "}
             <Image
               alt="Vercel Logo"
               className={styles.vercelLogo}
@@ -90,8 +90,8 @@ export default function Page(): JSX.Element {
                 alt="Turborepo"
                 height={614}
                 src="circles.svg"
-                style={{ pointerEvents: 'none' }}
                 width={614}
+                style={{ pointerEvents: "none" }}
               />
             </div>
             <div className={styles.logoGradientContainer}>
@@ -104,8 +104,8 @@ export default function Page(): JSX.Element {
                 height={120}
                 priority
                 src="turborepo.svg"
-                style={{ pointerEvents: 'none' }}
                 width={120}
+                style={{ pointerEvents: "none" }}
               />
             </div>
           </div>
